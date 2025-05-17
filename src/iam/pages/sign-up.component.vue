b20b758a89e06adf269138ed9c20627c04017595
petitavo <gustavohuanca1020@gmail.com>
2025-05-16 20:20:36 -0500

    feat(develop): recaptcha added.

--- src/iam/pages/sign-up.component.vue
+++ src/iam/pages/sign-up.component.vue
@@ -1,22 +1,44 @@
 <script>
-import {useAuthenticationStore} from "../services/authentication.store.js";
-import {SignUpRequest} from "../model/sign-up.request.js";
+import { useAuthenticationStore } from "../services/authentication.store.js";
+import { SignUpRequest } from "../model/sign-up.request.js";
 import ToolbarHome from "../../public/component/toolbar-home.component.vue";
 
 export default {
   name: "sign-up",
-  components: {ToolbarHome},
+  components: { ToolbarHome },
   data() {
     return {
       authenticationStore: useAuthenticationStore(),
       username: "",
       password: "",
-      role: ""
+      role: "",
+      passwordValidations: {
+        length: false,
+        upper: false,
+        lower: false,
+        number: false,
+        special: false,
+      }
     };
+  },
+  computed: {
+    isPasswordValid() {
+      return Object.values(this.passwordValidations).every(v => v === true);
+    }
+  },
+  watch: {
+    password(value) {
+      this.passwordValidations.length = value.length >= 8;
+      this.passwordValidations.upper = /[A-Z]/.test(value);
+      this.passwordValidations.lower = /[a-z]/.test(value);
+      this.passwordValidations.number = /[0-9]/.test(value);
+      this.passwordValidations.special = /[^A-Za-z0-9]/.test(value);
+    }
   },
   methods: {
     onSignUp() {
-      let signUpRequest = new SignUpRequest(this.username, this.password,this.role);
+      if (!this.isPasswordValid) return;
+      const signUpRequest = new SignUpRequest(this.username, this.password, this.role);
       this.authenticationStore.signUp(signUpRequest, this.$router);
     }
   }
@@ -32,6 +54,7 @@ export default {
   <div>
     <form @submit.prevent="onSignUp">
       <div class="p-fluid">
+        <!-- Username -->
         <div class="field mt-5">
           <pv-float-label>
             <label for="username">Username</label>
@@ -39,26 +62,38 @@ export default {
             <small v-if="!username" class="p-invalid">Username is required.</small>
           </pv-float-label>
         </div>
+
+        <!-- Password -->
         <div class="field mt-5">
           <pv-float-label>
             <label for="password">Password</label>
-            <pv-input-text id="password" v-model="password" :class="{'p-invalid': !password }" type="password"/>
-            <small v-if="!password" class="p-invalid">Password is required.</small>
+            <pv-input-text id="password" v-model="password" type="password" :class="{'p-invalid': !isPasswordValid}"/>
           </pv-float-label>
+          <ul class="validation-list">
+            <li :class="{ valid: passwordValidations.length }">At least 8 characters</li>
+            <li :class="{ valid: passwordValidations.upper }">At least one uppercase letter</li>
+            <li :class="{ valid: passwordValidations.lower }">At least one lowercase letter</li>
+            <li :class="{ valid: passwordValidations.number }">At least one number</li>
+            <li :class="{ valid: passwordValidations.special }">At least one special character (!, @, #, etc.)</li>
+          </ul>
         </div>
+
+        <!-- Role -->
         <div class="field mt-5">
           <pv-float-label>
             <label for="role">Role</label>
             <pv-dropdown id="role" v-model="role" :options="['Winemaker']"/>
             <small v-if="!role" class="p-invalid">Role is required.</small>
           </pv-float-label>
+        </div>
+
+        <!-- Buttons -->
         <div class="p-field mt-5">
-          <pv-button type="submit">Sign Up</pv-button>
+          <pv-button type="submit" :disabled="!username || !role || !isPasswordValid">Sign Up</pv-button>
           <router-link to="/home">
             <pv-button class="ml-2">Home</pv-button>
           </router-link>
         </div>
-      </div>
       </div>
     </form>
   </div>
@@ -67,5 +102,17 @@ export default {
 <style scoped>
 .ml-2 {
   margin-left: 0.5rem;
+}
+.validation-list {
+  list-style-type: none;
+  padding-left: 0.5rem;
+  margin-top: 0.5rem;
+}
+.validation-list li {
+  font-size: 0.85rem;
+  color: red;
+}
+.validation-list li.valid {
+  color: green;
 }
 </style>
